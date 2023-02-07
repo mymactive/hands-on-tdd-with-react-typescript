dc751e0b67a83a9ccf8103423a8fea0ddd17b623
@mymactive <mymactive@gmail.com>
2023-02-07 23:55:47 +0900

    refactor: テストの説明をより叙述的にする

--- src/hooks/index.spec.ts
+++ src/hooks/index.spec.ts
@@ -2,7 +2,22 @@ import { expect, test } from "vitest";
 import { renderHook, act } from "@testing-library/react";
 import { useBadge } from ".";
 
-test("attachを呼ぶと、attachedに変化する", () => {
+test("バッジが外れた状態から、外すとバッジは外れている", () => {
+  // 準備(arrange)
+  const { result } = renderHook(() => useBadge());
+
+  // 実行(act)
+  act(() => {
+    result.current.detach();
+  });
+
+  // 検証(assert)
+  expect(result.current.badge).toStrictEqual({
+    mode: "detached",
+  });
+});
+
+test("バッジが外れた状態から、バッジを付けるとバッジは付いている", () => {
   // 準備(arrange)
   const { result } = renderHook(() => useBadge());
 
@@ -18,7 +33,7 @@ test("attachを呼ぶと、attachedに変化する", () => {
   });
 });
 
-test("最後にdetachを呼ぶと、detachedに変化する", () => {
+test("バッジが付いている状態から、バッジを外すと外れている", () => {
   // 準備(arrange)
   const { result } = renderHook(() => useBadge());
   act(() => {
